78c1dc030f3ccad43c989ba570abdcffe9bb6a5b
doctordickbaker <doctordickbaker@gmail.com>
2015-10-18 03:20:50 -0500

    The for loop introduction
    It works!  and.. nothing else.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,31 @@
 /// #order #vidInfo #vidLength
+
+var feed = document.getElementById("feed");
+var list = ["test0","test1","test2"];
+
+
+function listCreate() {
+  for(var x=0;x<list.length;x++){
+    var card = document.createElement('div');
+    card.className = "card";
+    feed.appendChild(card);
+    
+    var order = document.createElement('div');
+    order.className = "order";
+    order.textContent = list[x];
+    card.appendChild(order);
+    
+    var vidInfo = document.createElement('div');
+    vidInfo.className = "vidInfo";
+    vidInfo.textContent = list[x];
+    card.appendChild(vidInfo);
+    
+    var vidLength = document.createElement('div');
+    vidLength.className="vidLength";
+    vidLength.textContent = list[x];
+    card.appendChild(vidLength);
+  }
+};
+
+
+listCreate();
