8774fa5b209894ad13b885c440818cde9f943de3
doctordickbaker <doctordickbaker@gmail.com>
2015-10-19 10:14:34 -0500

    I tried and failed...
    arg

--- js/index.js
+++ js/index.js
@@ -8,41 +8,50 @@ var userSubmit = document.getElementById('userSubmit');
 
 
 /////// List creator. Called by other functions when the list needs to be recreated////////
+
 function listCreate() {
+  
+  console.log("Creating list");
     feed.innerHTML = "";
   for(var x=1;x<list.length;x++){
-    console.log(x);
-    var card = document.createElement('div');
+    
+    var card = document.createElement('div');  /// create the card itself,  which contains the below divs.
     card.className = "card";
     card.id =  "card" + x;
     feed.appendChild(card);
     
-    var order = document.createElement('div');
+    var order = document.createElement('div');   ///create ordered number (place in list)
     order.className = "order";
     order.textContent = x;
     card.appendChild(order);
     
-    var vidInfo = document.createElement('div');
+    var vidInfo = document.createElement('div');  /// Create video info (title)///
     vidInfo.className = "vidInfo";
     vidInfo.textContent = list[x];
     card.appendChild(vidInfo);
     
-    //var vidLength = document.createElement('div');
+    //var vidLength = document.createElement('div');   ///  create Video length
     //vidLength.className="vidLength";
     //vidLength.textContent = list[x];
     //card.appendChild(vidLength);
     
-    var delButton = document.createElement('div');
+    var delButton = document.createElement('div');  /////create delete button
     delButton.className = "fontawesome-minus";
     card.appendChild(delButton);
     
-    delButton.onclick= function deleteCard() {
-      
-    }
+    function deleteCard(x) {
+      console.log(x);
+      list.splice(x,1);
+      listCreate();
+    };
+    
+    delButton.onclick = function(){
+      deleteCard(x);
+    };
+    
+    
   };
 };
-
-
 
 function user() {
   list[list.length] = userInput.value;
@@ -51,7 +60,10 @@ function user() {
 
 
 
+userSubmit.onclick = function() {
+  user();
+};
 
-//////////initilization stuff///////////
-userSubmit.onclick = user;
 listCreate();
+
+/// note ON HOW TO REMOVE CARD:  remove list[x]
