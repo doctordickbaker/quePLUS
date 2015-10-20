8f19b504a3515723d27cced8ebe4890be3503c0d
Anirban Mukhopadhyay <anirbanmu@outlook.com>
2015-10-20 04:19:45 -0400

    Quick fix to force x capture by value

--- js/index.js
+++ js/index.js
@@ -10,42 +10,43 @@ var userSubmit = document.getElementById('userSubmit');
 /////// List creator. Called by other functions when the list needs to be recreated////////
 
 function listCreate() {
-  
+
     console.log("Creating list");
- 
+
     feed.innerHTML = "";
     for(var x=1;x<list.length;x++){
-    
+
     var card = document.createElement('div');  /// create the card itself,  which contains the below divs.
     card.className = "card";
     card.id =  "card" + x;
     feed.appendChild(card);
-    
+
     var order = document.createElement('div');   ///create ordered number (place in list)
     order.className = "order";
     order.textContent = x;
     card.appendChild(order);
-    
+
     var vidInfo = document.createElement('div');  /// Create video info (title)///
     vidInfo.className = "vidInfo";
     vidInfo.textContent = list[x];
     card.appendChild(vidInfo);
-    
+
     //var vidLength = document.createElement('div');   ///  create Video length
     //vidLength.className="vidLength";
     //vidLength.textContent = list[x];
     //card.appendChild(vidLength);
-    
+
     var delButton = document.createElement('div');  /////create delete button
     delButton.className = "fontawesome-minus";
     card.appendChild(delButton);
-    
-    delButton.onclick = function(x){
-      list.splice(x,1);
-      listCreate();
-    };
-    
-    
+
+    delButton.onclick = function(currentLoopIterationX) {
+      return function() {
+        list.splice(currentLoopIterationX,1);
+        listCreate();
+      }
+    }(x);
+
   };
 };
 
